b38186650b5245d17f8e2b4a12ac914d20b54df5
Tobias Pfeiffer <pragtob@gmail.com>
2014-11-18 11:22:02 +0100

    display German language

--- config.rb
+++ config.rb
@@ -16,7 +16,7 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
-set :displayed_languages, %i(en es pl)
+set :displayed_languages, %i(en de es pl)
 
 configure :build do
   activate :minify_css
